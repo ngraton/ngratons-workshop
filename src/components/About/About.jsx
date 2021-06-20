ab832e411bf253c2ff59eb429145d02b433631d0
Nic Graton <mail@ngraton.dev>
2021-06-20 08:44:01 -0400

    move intro to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import './About.css'
 
-function About() {
+export default function Intro() {
+
+  const [animationComplete, setAnimationComplete] = React.useState(false)
+
   return (
     <div className="about">
-      <h1>
-        Edit About component or pages/about.jsx to include your information.
-      </h1>
+      <div className="content">
+        <aside className="side">
+          <div className="picture">
+            <div className="picture-shadow" />
+            <StaticImage
+              className="me"
+              src="../../../static/images/me.jpg"
+              loading="eager"
+              height={400}
+              width={400}
+              alt="me"
+            />
+          </div>
+        </aside>
+        <div className="about-div">
+          <div className="about-text"/>
+          <p id="about-line-1">
+            <span id="about-hi">Hi. </span><span id="about-i-am">I&apos;m Nic.</span>
+          </p>
+          <p id="about-line-2">I&apos;m a software engineer and maker. </p>
+          <p id="about-line-3" onAnimationEnd={() => setAnimationComplete(true)}>Welcome to my virtual workshop. </p>
+        </div>
+        <div className={animationComplete ? 'arrow' : ''}>
+          <span/>
+          <span/>
+          <span/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default About;
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,41 +1,13 @@
-import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import './Intro.css'
+import React from "react";
 
-export default function Intro() {
-
-  const [animationComplete, setAnimationComplete] = React.useState(false)
-
+function About() {
   return (
     <div className="intro">
-      <div className="content">
-        <aside className="side">
-          <div className="picture">
-            <div className="picture-shadow" />
-            <StaticImage
-              className="me"
-              src="../../../static/images/me.jpg"
-              loading="eager"
-              height={400}
-              width={400}
-              alt="me"
-            />
-          </div>
-        </aside>
-        <div className="intro-div">
-          <div className="intro-text"/>
-          <p id="intro-line-1">
-            <span id="intro-hi">Hi. </span><span id="intro-i-am">I&apos;m Nic.</span>
-          </p>
-          <p id="intro-line-2">I&apos;m a software engineer and maker. </p>
-          <p id="intro-line-3" onAnimationEnd={() => setAnimationComplete(true)}>Welcome to my virtual workshop. </p>
-        </div>
-        <div className={animationComplete ? 'arrow' : ''}>
-          <span/>
-          <span/>
-          <span/>
-        </div>
-      </div>
+      <h1>
+        Edit About component or pages/about.jsx to include your information.
+      </h1>
     </div>
-  )
+  );
 }
+
+export default About;
