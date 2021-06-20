0c7f3c488393cbbf52f338552bd730605fa3ca2e
Nic Graton <mail@ngraton.dev>
2021-06-20 08:25:47 -0400

    add intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,13 +3,39 @@ import { StaticImage } from "gatsby-plugin-image"
 import './Intro.css'
 
 export default function Intro() {
+
+  const [animationComplete, setAnimationComplete] = React.useState(false)
+
   return (
     <div className="intro">
-    <StaticImage className="wallpaper" src="../../static/images/cleveland.jpeg"/>
-      <h1>
-        Edit About component or pages/about.jsx to include your information.
-      </h1>
+      <div className="content">
+        <aside className="side">
+          <div className="picture">
+            <div className="picture-shadow" />
+            <StaticImage
+              className="me"
+              src="../../../static/images/me.jpg"
+              loading="eager"
+              height={400}
+              width={400}
+              alt="me"
+            />
+          </div>
+        </aside>
+        <div className="intro-div">
+          <div className="intro-text"/>
+          <p id="intro-line-1">
+            <span id="intro-hi">Hi. </span><span id="intro-i-am">I&apos;m Nic.</span>
+          </p>
+          <p id="intro-line-2">I&apos;m a software engineer and maker. </p>
+          <p id="intro-line-3" onAnimationEnd={() => setAnimationComplete(true)}>Welcome to my virtual workshop. </p>
+        </div>
+        <div className={animationComplete ? 'arrow' : ''}>
+          <span/>
+          <span/>
+          <span/>
+        </div>
+      </div>
     </div>
   )
 }
-
