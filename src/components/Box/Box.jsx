7939f2776600188b8fbdbc9e7c8f66da1b57ccb0
Nic Graton <mail@ngraton.dev>
2021-07-27 18:46:11 -0400

    add box

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Box({ title, children, footer}) {
+  return (
+    <div className="box-border">
+      <h1 className="box-title">
+        {title}
+      </h1>
+      <div className="box">
+        <div className="box-content">
+          <div>
+            {children}
+          </div>
+        </div>
+      </div>
+      <div className="box-footer">
+        <span className="box-footer-text">
+          {footer || 'ngraton.dev'}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Box;
