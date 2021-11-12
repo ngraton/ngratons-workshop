c9525e375ffbb9be30cfad9c39a0e08bf172b9b2
Firstname Lastname <you@example.com>
2021-11-11 23:54:02 -0500

    add projects query

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Box from '../Box/Box'
 
-function Projects({ projects }) {
-  return (
-    <div>
-      <h1 className="title">
-        Projects
-      </h1>
-      <div>
-        {
-          
+
+function Projects() {
+  const data = useStaticQuery(
+    graphql`
+      query ProjectsQuery {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                project
+              }
+            }
+          }
         }
+      }
+    `
+  )
+
+  const projects = []
+    data.allMarkdownRemark.edges.forEach((edge) => {
+      if (!projects.includes(edge.node.frontmatter.project)) {
+        projects.push(edge.node.frontmatter.project)
+      }
+    })
+
+  const projectList = projects.map((project) => (
+      <p key={project}>{project}</p>
+    )
+  )
+
+  return (
+    <Box title='Projects'>
+      <div>
+        {projectList}
       </div>
-    </div>
-  );
+    </Box>
+  )
 }
 
-export default Projects;
+export default Projects
